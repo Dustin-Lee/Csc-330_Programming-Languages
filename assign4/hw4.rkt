8e69ed08df1cebfeb1b3b6d0248a4f94b4466c12
Dustin Chang <1022dustin@gmail.com>
2015-02-17 21:35:02 -0800

    done3

--- assign4/hw4.rkt
+++ assign4/hw4.rkt
@@ -5,10 +5,29 @@
 
 ;; put your code below
 
+;; 1
 (define (sequence low high stride)
   (if (<= low high)
       (cons low (sequence (+ low stride) high stride))
       '()))
 
+;; 2
 (define (string-append-map xs suffix)
   (map (lambda (x) (string-append x suffix)) xs))
+
+;; 3
+(define (list-nth-mod xs n)
+  (cond [(< n 0) (error "list-nth-mod: negative number")]
+        [(null? xs) (error "list-nth-mod: empty list")]
+        [#t (let ([r (remainder n (length xs))]) (car (list-tail xs r)))]))
+              
+
+
+
+;
+;         (list-nth-mod (list-tail xs 1) (- n 1))]))
+;
+;(define (list-nth-mod xs n) 
+;  (cond [(< n 0) (error "list-nth-mod: negative number")]
+;        [(null? xs) (error "list-nth-mod: empty list")]
+;        [#t (let ([rem (remainder n (length xs))]) (car (list-tail xs rem)))]))
