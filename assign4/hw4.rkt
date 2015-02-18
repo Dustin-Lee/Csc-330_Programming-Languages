7efd9d769ba95ff650cdc7f744f145b6ca6b9bd0
Dustin Chang <1022dustin@gmail.com>
2015-02-17 22:15:55 -0800

    done4

--- assign4/hw4.rkt
+++ assign4/hw4.rkt
@@ -20,3 +20,11 @@
   (cond [(< n 0) (error "list-nth-mod: negative number")]
         [(null? xs) (error "list-nth-mod: empty list")]
         [#t (let ([r (remainder n (length xs))]) (car (list-tail xs r)))]))
+
+;; 4
+;; stream, number --> rtns a list
+(define (stream-for-n-steps s n)
+  (if (= n 0) ;or equal?
+      '()
+      (let ([stm (s)])
+        (cons (car stm) (stream-for-n-steps (cdr stm) (- n 1))))))
