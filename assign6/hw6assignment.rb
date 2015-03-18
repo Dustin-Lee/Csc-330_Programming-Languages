250717773a13ea5337657bd46db84580888dfe90
Dustin Chang <1022dustin@gmail.com>
2015-03-18 15:56:26 -0700

    Took out all testing lines

--- assign6/hw6assignment.rb
+++ assign6/hw6assignment.rb
@@ -5,21 +5,6 @@
 # part of your solution.
 
 class MyPiece < Piece
-  # The constant All_My_Pieces should be declared here
-  # your enhancements here
-=begin
-  def move (delta_x, delta_y, delta_rotation)
-  	moved = true
-  	potential = @all_rotations[(@rotation_index + delta_rotation) % @all_rotations.size]
-    if potential != 0
-      potential.each{|posns| 
-        if !(@board.empty_at([posns[0] + delta_x + @base_position[0],
-          posns[1] + delta_y + @base_position[1]]));
-          moved = false;
-        end
-      }
-    else
-=end
   Cheat_Piece = [[[[0, 0]]]]
 
   All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
@@ -46,13 +31,9 @@ class MyPiece < Piece
 		puts 'In next_piece2.1'
 		MyPiece.new(All_My_Pieces.sample, board)
 	end
-
 end	#End of MyPiece
 
-
-
 class MyBoard < Board
-  # your enhancements here
   def initialize(game)
   	@grid = Array.new(num_rows) {Array.new(num_columns)}
     @current_block = MyPiece.next_piece(self)
@@ -60,33 +41,25 @@ class MyBoard < Board
     @game = game
     @delay = 500
     @board_cheat_status = false
-    puts 'In MyBoard'
   end
   
   def next_piece
   	if @board_cheat_status
-  		puts 'In next_piece2.cheat'
   		@current_block = MyPiece.next_piece_cheat(self)
-  		puts 'AFTER In next_piece2.cheat1'
   		@current_pos = nil
-  		puts 'AFTER In next_piece2.cheat2'
   		@board_cheat_status = false
-  		puts 'AFTER In next_piece2.cheat3'
   	else
   		@current_block = MyPiece.next_piece(self)
   		@current_pos = nil
-  		puts 'In next_piece2.2'
   	end
   end
 
   def remove_delay	#To remove repeatable code within store_current
-  	#@grid[current[1]+displacement[1]][current[0]+displacement[0]] = @current_pos[index]}
   	remove_filled
 	  @delay = [@delay - 2, 80].max
 	end
 
   def store_current
-  	puts 'In store_current2'
   	locations = @current_block.current_rotation
   	block_cells_size = locations.size
   	puts block_cells_size
@@ -107,7 +80,6 @@ class MyBoard < Board
   		@grid[current[1]+displacement[1]][current[0]+displacement[0]] = @current_pos[index]}
   		remove_delay
 	  else
-      puts 'In 5'
 	  	displacement = @current_block.position
 	  	(0..4).each{|index| current = locations[index];
 	  	@grid[current[1]+displacement[1]][current[0]+displacement[0]] = @current_pos[index]}
@@ -116,37 +88,25 @@ class MyBoard < Board
   end #End of store_current
 
   def update_cheat_score
-    puts 'In Update Cheat'
     if @board_cheat_status
       puts 'Already cheated'
-      #next #Have cheated once already
     else  #Haven't cheated yet
-      puts 'In else cheat'
       if @score >= 100
-        puts '>=100'
         @board_cheat_status = true
         puts @board_cheat_status
         @score = @score - 100
-        @game.update_score  #Might not need
-        puts 'Updating CHEAT'
+        #@game.update_score
       end
     end
-    #@current_block = MyPiece.next_piece_cheat(self)
-    #@current_pos = nil
   end #End of update_cheat_score
-
 end #End of MyBoard
 
-
-
 class MyTetris < Tetris
-  # your enhancements here
-  #@@cheat_status = false
   attr_accessor :board
   def key_bindings
   	super
   	@root.bind('u', lambda {@board.rotate_clockwise	#Binds 'u' to rotate 180 deg
-  													@board.rotate_clockwise})
+                            @board.rotate_clockwise})
   	@root.bind('c', lambda {self.cheat}) #For cheat piece
   end
 
@@ -159,19 +119,7 @@ class MyTetris < Tetris
   end
 
   def cheat
-  	puts 'In cheat def'
-  	#if @@cheat_status == false
-  		#puts 'Passed FALSE'
-  		#puts @board.score
-  		#if @board.score >= 100
-  	#		puts 'In >= 100'
-  	#		@@cheat_status = true	#To make sure it can't be called multiple times per block
   	@board.update_cheat_score
-
-  			
-  			puts 'AFTER2'
-  	#	end
-  	#end
   end
 end #End of MyTetris
 
